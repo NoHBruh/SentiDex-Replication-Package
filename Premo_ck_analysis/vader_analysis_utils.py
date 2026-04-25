74d38c96e01afb389e63b2cdb285b16ad2750f4b
NoHBruh <115785353+NoHBruh@users.noreply.github.com>
2026-04-25 07:02:40 +0200

    Update vader_analysis_utils.py

--- Premo_ck_analysis/vader_analysis_utils.py
+++ Premo_ck_analysis/vader_analysis_utils.py
@@ -5,14 +5,14 @@ def sentiment_score(comment) :
     sentiment_dict = sia.polarity_scores(comment)
         
     compound = sentiment_dict['compound']
-    if compound > 0.05:
+    if compound > 0.5:
         sentiment_dict['Sentiment'] = "Positive"
         
    
-    elif compound < -0.05:
+    elif compound < -0.5:
         sentiment_dict['Sentiment'] = "Negative"
     
-    elif compound <= 0.05 and compound >= -0.05 :
+    elif compound <= 0.5 and compound >= -0.5 :
         sentiment_dict['Sentiment'] = "Neutral"
         
     return sentiment_dict
